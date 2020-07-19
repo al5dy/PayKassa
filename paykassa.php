72a0fca0915086bc01cd6369cdf54e5adb8cab6d
Anton <ant.lokotkov@gmail.com>
2020-07-20 02:00:54 +0300

    Update main SCI class

--- paykassa.php
+++ paykassa.php
@@ -6,10 +6,8 @@
  * Version: 1.0.1
  * Author: al5dy
  * Author URI: https://ziscod.com
- *
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
-
  * Text Domain: paykassa
  * Domain Path: /languages/
  *
@@ -17,32 +15,31 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+  exit; // Exit if accessed directly.
 }
 
 // Define PK_PLUGIN_FILE.
 if ( ! defined( 'PK_PLUGIN_FILE' ) ) {
-	define( 'PK_PLUGIN_FILE', __FILE__ );
+  define( 'PK_PLUGIN_FILE', __FILE__ );
 }
 
 // Include the main PayKassa class.
 if ( ! class_exists( 'PayKassa' ) ) {
-	include_once dirname( __FILE__ ) . '/includes/class-paykassa.php';
+  include_once dirname( __FILE__ ) . '/includes/class-paykassa.php';
 }
 
 /**
  * Main instance of PayKassa.
- *
  * Returns the main instance of PayKassa to prevent the need to use globals.
  *
- * @since  1.0.0
  * @return PayKassa
+ * @since  1.0.0
  */
 if ( ! function_exists( 'paykassa' ) ) {
-	function paykassa() {
-		return PayKassa::instance();
-	}
+  function paykassa() {
+    return PayKassa::instance();
+  }
 
-	// Global for backwards compatibility.
-	$GLOBALS['paykassa'] = paykassa();
+  // Global for backwards compatibility.
+  $GLOBALS['paykassa'] = paykassa();
 }
